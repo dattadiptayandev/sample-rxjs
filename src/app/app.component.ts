533fa0ecaf5b1e7cc7caceaf0bc15ae94ad7a906
Diptayan Datta <dattadiptayandev@gmail.com>
2021-01-21 23:51:31 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sample-rxjs';
+  observable$;
+
+  ngOnInit() {
+    this.observable$ = Observable.create((observer) => {
+      observer.next(1);
+      observer.next(3);
+      observer.next(8);
+      observer.complete();
+    });
+
+    this.observable$.subscribe(
+      value => console.log(value),
+      err => { },
+      () => console.log("this is the end")
+    );
+  }
+
+  ngOnDestroy() {
+    this.observable$.unsubscribe();
+  }
 }
